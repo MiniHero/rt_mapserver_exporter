cea15f6dab4859d94e95ecb747f900d1db4a230b
Yuchuan Zhou <MiniHero@Yuchuans-MacBook-Pro.local>
2015-10-25 18:38:53 -0400

    temp

--- ui/interface.py
+++ ui/interface.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mapfileexportdlg.ui'
 #
-# Created: Sat Sep  5 20:43:34 2015
+# Created: Sun Oct 25 18:00:34 2015
 #      by: PyQt4 UI code generator 4.11.1
 #
 # WARNING! All changes made in this file will be lost!
